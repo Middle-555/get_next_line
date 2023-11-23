4355685514e84b76032007586da2a0e7ccf862e9
Middle <pourcel.kevin@gmail.com>
2023-11-24 00:15:58 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:04:16 by kpourcel          #+#    #+#             */
-/*   Updated: 2023/11/24 00:09:58 by kpourcel         ###   ########.fr       */
+/*   Updated: 2023/11/24 00:15:48 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*read_and_stock(int fd, char *buff)
 	int		already_read;
 
 	if (!buff) /* Si le buff est vide on le créer et lui alloue 1 octet pour pouvoir join par la suite. */ 
-		buff == ft_calloc(sizeof(char) * 1,1);
-	stash == ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+		buff = ft_calloc(1,1);
+	stash = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	already_read = 1; /* On initialise à 1 pour avoir au minimum 1 itération. */ 
 	while(already_read > 0)
 	{
@@ -53,7 +53,7 @@ char	*ft_seg_line(char *stash)
 		i++;
 	}
 	new_line = i + 2; /* +2 car ajout du \n et \0*/
-	line == ft_calloc(new_line, sizeof(char));
+	line = ft_calloc(new_line, sizeof(char));
 	while (!stash[i] && stash[i] != '\n')
 	{
 		line[i] = stash[i];
@@ -90,15 +90,17 @@ char	*ft_clear_and_save(char *stash)
 char	*get_next_line(int fd)
 {
 	static char	*buffer[4096];
+	int		i;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd,0,0) == -1)
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
 		return (free(buffer), buffer == NULL);
-	buffer == read_and_stock(fd, buffer);
+	buffer[i] = read_and_stock(fd, buffer);
 	if (!buffer)
 		return (NULL);
-	buffer == ft_clear_and_save(buffer);
-	line == ft_seg_line(buffer);
+	buffer[i] = ft_clear_and_save(buffer);
+	line = ft_seg_line(buffer);
 	return (line);	
-}// 4.
-// read écrase le buffer à chaque fois qu'elle repasse il faut donc stocker ce que l'on met dans le buffer dans une variable statique 
+}
+ 
