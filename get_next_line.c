c6fc7f07707ef91eb84538d5c1c33cd48356c4a8
Middle <pourcel.kevin@gmail.com>
2023-11-24 20:29:26 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kpourcel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:04:16 by kpourcel          #+#    #+#             */
-/*   Updated: 2023/11/24 20:24:53 by kpourcel         ###   ########.fr       */
+/*   Updated: 2023/11/24 20:29:12 by kpourcel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,10 @@ char	*get_next_line(int fd)
 
 	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
-		return (free(buffer), buffer == NULL, NULL);
+	{
+		free (buffer);
+		return (NULL);
+	}
 	buffer[i] = read_and_stock(fd, buffer);
 	if (!buffer)
 		return (NULL);
